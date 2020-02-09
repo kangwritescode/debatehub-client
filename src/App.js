290e4b2c0128e60fc43668cd5dc6e5c0edf1773e
kangwritescode <kangwritescode@gmail.com>
2020-02-08 17:11:42 -0800

    auth-hook

--- src/App.js
+++ src/App.js
@@ -13,62 +13,10 @@ import UpdatePlace from './places/pages/UpdatePlace'
 import Auth from './user/pages/Auth'
 import MainNavigation from './shared/components/Navigation/MainNavigation'
 import { AuthContext } from './shared/context/auth-context'
-
-let logoutTimer
+import useAuth from './shared/hooks/auth-hook'
 
 const App = () => {
-  const [token, setToken] = useState(false)
-  const [userId, setUserId] = useState(false)
-  const [tokenExpDate, setTokenExpDate] = useState()
-
-  const login = useCallback((uid, token, expirationDate) => {
-    setToken(token)
-    setUserId(uid)
-    const tokenExpirationDate =
-      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60)
-    setTokenExpDate(tokenExpirationDate)
-    localStorage.setItem(
-      'userData',
-      JSON.stringify({
-        token: token,
-        userId: uid,
-        expiration: tokenExpirationDate.toISOString()
-      })
-    )
-  }, [])
-
-  const logout = useCallback(() => {
-    setToken(null)
-    setUserId(null)
-    setTokenExpDate(null)
-    localStorage.removeItem('userData')
-  }, [])
-
-  useEffect(() => {
-    if (token && tokenExpDate) {
-      const remainingTime = tokenExpDate.getTime() - new Date().getTime()
-      logoutTimer = setTimeout(logout, remainingTime)
-    }
-    return () => {
-      clearTimeout(logoutTimer)
-    }
-  }, [token, logout, tokenExpDate])
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem('userData'))
-    if (
-      storedData &&
-      storedData.token &&
-      new Date(storedData.expiration) > new Date()
-    ) {
-      login(
-        storedData.userId,
-        storedData.token,
-        new Date(storedData.expiration)
-      )
-    }
-  }, [login])
-
+  const { token, login, logout, userId } = useAuth()
   let routes
 
   if (token) {
