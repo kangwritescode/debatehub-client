1e5da431ddac41508d298c0baece944bbe3f0f2b
kangwritescode <kangwritescode@gmail.com>
2020-02-14 19:41:27 -0800

    ui bug

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -10,8 +10,8 @@ const PlaceList = props => {
     return (
       <div className="place-list center">
         <Card>
-          <h2>No places found. Maybe create one?</h2>
-          <Button to="/places/new">Share Place</Button>
+          <h2>No places found.</h2>
+          {/* <Button to="/places/new">Share Place</Button> */}
         </Card>
       </div>
     );
